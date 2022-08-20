42024500d8afdd626523d88a25e0813679de98c9
yujiale <yujiale@authing.cn>
2022-08-20 15:55:58 +0800

    feat: stream suspension operation method

--- src/test/java/com/java/demo/streamapi/StreamApiMiddleOperationTest.java
+++ src/test/java/com/java/demo/streamapi/StreamApiMiddleOperationTest.java
@@ -1,9 +1,44 @@
 package com.java.demo.streamapi;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 /**
  * @Author yujiale
  * @Date 2022/8/20 15:51
- * @Description stream middle operation method
+ * @Description stream intermediate operation method
+ * <p>
+ * 方法名	说明
+ * map	将对应的元素使用给定方法进行转换
+ * mapToInt(mapToLong,mapToDouble)	将对应的元素使用给定方法进行转换(返回类型必须是 int,long,double)
+ * flatMap	如果流的元素为数组或者Collection，flatMap就是将每个Object[]元素或Collection<Object>元素都转换为Object元素
+ * filter	通过设置条件来过滤元素
+ * distinct	集合中的元素去重
+ * sorted	将集合中的元素排序
+ * peek	生成一个包含原Stream的所有元素的新Stream，同时会提供一个消费函数即引用的方法A，当Stream每个元素被消费的时候都会先执行新Stream给定的方法A。peek是中间操作，如果pee后没有最终操作，则peek不会执行
+ * limit	返回Stream的前n个元素
+ * skip	删除Stream的前n个元素
  **/
+@Slf4j
+@DataJpaTest
 class StreamApiMiddleOperationTest {
+
+    private Long startTime;
+
+
+    @BeforeEach()
+    void before() {
+        startTime = System.currentTimeMillis();
+        log.info("method run startTime:" + startTime);
+    }
+
+    @AfterEach()
+    void after() {
+        Long endTime = System.currentTimeMillis();
+        log.info("method run endTime:" + endTime);
+        log.info(String.format("exercise 1 - execution time: %1$d ms", (endTime - startTime)));
+
+    }
 }
--- src/test/java/com/java/demo/streamapi/StreamApiSuspensionOperationTest.java
+++ src/test/java/com/java/demo/streamapi/StreamApiSuspensionOperationTest.java
@@ -1,9 +1,53 @@
 package com.java.demo.streamapi;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 /**
  * @Author yujiale
  * @Date 2022/8/20 15:52
- * @Description stream suspension operation method
+ * @Description stream terminal operation method
+ *
+ * orEach	遍历Stream中的每个元素
+ * forEachOrdered	遍历Stream中的每个元素 区别： 在串行流（stream）中没有区别，在并行流（parallelStream）中如果数据源是有序集合，forEachOrdered输出顺序与数据源中顺序一致，forEach则是乱序。
+ * toArray	将流转换为Object[]或者指定类型的数组
+ * reduce	将集合中的每个元素聚合成一条数据
+ * collect	将流转换成集合或聚合元素
+ * min	获取集合中最小值
+ * max	获取集合中最大值
+ * count	获取集合中元素个数
+ * anyMatch	Stream 中任意一个元素符合传入的 predicate，返回 true
+ * allMatch	Stream 中全部元素符合传入的 predicate，返回 true
+ * noneMatch	Stream 中没有一个元素符合传入的 predicate，返回 true
+ * findFirst	如果数据源是有序集合，返回Stream 中第一个元素的Optional对象，如果是无序集合，则返回Stream 中任意一个元素的Optional对象。
+ * findAny	返回Stream 中任意一个元素的Optional对象。
+ * iterator	返回一个无限元素的有序的Stream对象。
+ * builder	返回一个Builder对象，Builder对象在调用build()返回Stream对象。
+ * empty	返回一个空的有序的Stream对象。
+ * of	返回包含单个元素的有序的Stream对象。
+ * generate	返回一个无限元素的无序的的Stream对象。
+ * concat	将两个Stream连接成一个Stream。
  **/
+@Slf4j
+@DataJpaTest
 class StreamApiSuspensionOperationTest {
+
+    private Long startTime;
+
+
+    @BeforeEach()
+    void before() {
+        startTime = System.currentTimeMillis();
+        log.info("method run startTime:" + startTime);
+    }
+
+    @AfterEach()
+    void after() {
+        Long endTime = System.currentTimeMillis();
+        log.info("method run endTime:" + endTime);
+        log.info(String.format("exercise 1 - execution time: %1$d ms", (endTime - startTime)));
+
+    }
 }
