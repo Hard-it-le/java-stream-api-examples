848255ab0317fbd374b00eb1096a16df7176d913
yujiale <yujiale@authing.cn>
2022-08-20 15:53:33 +0800

    feat: stream suspension operation method

---
+++ src/test/java/com/java/demo/streamapi/StreamApiSuspensionOperationTest.java
@@ -0,0 +1,9 @@
+package com.java.demo.streamapi;
+
+/**
+ * @Author yujiale
+ * @Date 2022/8/20 15:52
+ * @Description stream suspension operation method
+ **/
+class StreamApiSuspensionOperationTest {
+}
